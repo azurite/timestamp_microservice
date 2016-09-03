cefe50e7af2c6096b9c1feb6855e5e2507270729
Marko Nikic <darkblue.azurite@gmail.com>
2016-09-03 15:33:29 +0000

    completed user stories

--- server.js
+++ server.js
@@ -1,9 +1,51 @@
 var express = require("express");
 var app = express();
 
+var zeroFill = function(n) {
+    return n < 10 ? "0" + n : n;
+};
+
+var parseDate = function(d) {
+    if(typeof d !== "string") {
+        return false;
+    }
+    
+    if(!isNaN(Number(d)) && (new Date(Number(d))).toString() !== "Invalid Date") {
+        return new Date(Number(d));
+    }
+    
+    if((new Date(d)).toString() !== "Invalid Date") {
+        return new Date(d);
+    }
+    
+    return false;
+};
+
+var format = function(d) {
+    var months = ["January", "February", "March", "Apirl", "May", "June", "July", "August", "September", "October", "November", "December"];
+    
+    var year = d.getFullYear();
+    var month = months[d.getMonth()];
+    var day = zeroFill(d.getDay());
+    
+    return month + " " + day + ", " + year;
+};
+
 app.get("/", (req, res) => {
     res.sendFile("client/index.html", { root: __dirname });
-    
+});
+
+app.get("/:date", (req, res) => {
+    var date = parseDate(req.params.date);
+    console.log(date);
+    if(date) {
+        res.json({
+            unix: req.params.date,
+            natural: format(date)
+        });
+        res.end();
+    }
+    res.end("Invalid Date Parameter");
 });
 
 app.listen(8080, () => {
